231d211fc573ca8d91ef92975af040140841becc
gregorydaval345 <davalgregory69@gmail.com>
2023-06-26 20:43:05 +0100

    Recipe page ui design completed

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import { BlurView } from "expo-blur";
 import { SIZES, FONTS, COLORS, icons } from "../constants";
+import { Viewers } from "../components";
 
 const HEADER_HEIGHT = 350;
 
@@ -134,6 +135,159 @@ const Recipe = ({ navigation, route }) => {
     );
   }
 
+  function renderHeaderBar() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 90,
+          flexDirection: "row",
+          alignItems: "flex-end",
+          justifyContent: "space-between",
+          paddingHorizontal: SIZES.padding,
+          paddingBottom: 10,
+        }}
+      >
+        {/* Screen Overlay */}
+        <Animated.View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: COLORS.black,
+            opacity: scrollY.interpolate({
+              inputRange: [HEADER_HEIGHT - 100, HEADER_HEIGHT - 70],
+              outputRange: [0, 1],
+            }),
+          }}
+        />
+
+        {/* Header Bar Title */}
+        <Animated.View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            alignItems: "center",
+            justifyContent: "flex-end",
+            paddingBottom: 10,
+            opacity: scrollY.interpolate({
+              inputRange: [HEADER_HEIGHT - 100, HEADER_HEIGHT - 50],
+              outputRange: [0, 1],
+            }),
+            transform: [
+              {
+                translateY: scrollY.interpolate({
+                  inputRange: [HEADER_HEIGHT - 100, HEADER_HEIGHT - 50],
+                  outputRange: [50, 0],
+                  extrapolate: "clamp",
+                }),
+              },
+            ],
+          }}
+        >
+          <Text style={{ color: COLORS.lightGray2, ...FONTS.body4 }}>
+            Recipe by:
+          </Text>
+          <Text style={{ color: COLORS.white2, ...FONTS.h3 }}>
+            {selectedRecipe?.author?.name}
+          </Text>
+        </Animated.View>
+        {/* Back button */}
+        <TouchableOpacity
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: 35,
+            width: 35,
+            borderRadius: 18,
+            borderWidth: 1,
+            borderColor: COLORS.lightGray,
+            backgroundColor: COLORS.transparentBlack5,
+          }}
+          onPress={() => navigation.goBack()}
+        >
+          <Image
+            source={icons.back}
+            style={{ width: 15, height: 15, tintColor: COLORS.lightGray }}
+          />
+        </TouchableOpacity>
+        {/* Bookmark button */}
+        <TouchableOpacity
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: 35,
+            width: 35,
+          }}
+        >
+          <Image
+            source={
+              selectedRecipe?.isBookmark ? icons.bookmarkFilled : icons.bookmark
+            }
+            style={{ width: 30, height: 30, tintColor: COLORS.darkGreen }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function renderRecipeInfo() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          height: 130,
+          width: SIZES.width,
+          paddingHorizontal: 30,
+          paddingVertical: 20,
+          alignItems: "center",
+        }}
+      >
+        {/* Recipe */}
+        <View style={{ flex: 1.5, justifyContent: "center" }}>
+          <Text style={{ ...FONTS.h2 }}>{selectedRecipe?.name}</Text>
+          <Text
+            style={{ marginTop: 5, color: COLORS.lightGray2, ...FONTS.body4 }}
+          >
+            {selectedRecipe?.duration} | {selectedRecipe?.serving}
+          </Text>
+        </View>
+
+        {/* Viewers */}
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Viewers viewersList={selectedRecipe?.viewers} />
+        </View>
+      </View>
+    );
+  }
+
+  function renderIngredientHeader() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          paddingHorizontal: 30,
+          marginTop: SIZES.radius,
+          marginBottom: SIZES.padding,
+        }}
+      >
+        <Text style={{ flex: 1, ...FONTS.h3 }}>Ingredients</Text>
+        <Text style={{ color: COLORS.lightGray2, ...FONTS.body4 }}>
+          {selectedRecipe?.ingredients.length} items
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
@@ -150,8 +304,9 @@ const Recipe = ({ navigation, route }) => {
             {/* Header */}
             {renderRecipeHeader()}
             {/* Info */}
-
+            {renderRecipeInfo()}
             {/* Ingredient Title */}
+            {renderIngredientHeader()}
           </View>
         }
         scrollEventThrottle={16}
@@ -198,7 +353,10 @@ const Recipe = ({ navigation, route }) => {
             </View>
           </View>
         )}
+        ListFooterComponent={<View style={{ marginBottom: 100 }}></View>}
       />
+      {/* Header Bar */}
+      {renderHeaderBar()}
     </View>
   );
 };
